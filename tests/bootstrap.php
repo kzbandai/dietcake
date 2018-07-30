b53f60653198d6a9ce1b340a7f67c3edc20457b7
kzbandai <nisigin@gmail.com>
2018-07-29 17:59:16 -0700

    Rename a file and lines

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,4 +3,4 @@ error_reporting(E_ALL | E_STRICT);
 
 define('ROOT_DIR', dirname(__DIR__) . '/');
 define('APP_DIR', ROOT_DIR . 'app/');
-require_once ROOT_DIR . 'dietcake.php';
+require_once ROOT_DIR . 'dietcook.php';
